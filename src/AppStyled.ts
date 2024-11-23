9af2ab10d098fd1aaac8bdff658582bbc893a947
Oleksandr-Berd <alex.berd86@gmail.com>
2024-11-23 13:07:31 +0200

    basic markup

---
+++ src/AppStyled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const AppCustom = styled.div`
+  height: 100vh;
+
+  text-align: center;
+
+  background-color: #19a1ae;
+`;
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,32 @@
+const Profile: React.FC = () => {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <div>
+          <div>
+            <h3>Victor Crest</h3>
+            <p>26</p>
+          </div>
+          <p>London</p>
+        </div>
+        <ul>
+          <li>
+            <h4>80K</h4>
+            <p>Followers</p>
+          </li>
+          <li>
+            <h4>803K</h4>
+            <p>Likes</p>
+          </li>
+          <li>
+            <h4>1.4K</h4>
+            <p>Photos</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
